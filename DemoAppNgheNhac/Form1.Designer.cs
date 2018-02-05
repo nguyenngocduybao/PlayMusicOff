850eb318c809b195688d2b63b062e3efafe2c74a
nndb05123 <duybao05123@gmail.com>
2018-02-05 13:57:12 +0700

    Demo

---
+++ DemoAppNgheNhac/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace DemoAppNgheNhac
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.btn_open = new System.Windows.Forms.Button();
+            this.listmusic = new System.Windows.Forms.ListBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.axWindowsMediaPlayer1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btn_open, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.listmusic, 0, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(-2, -2);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 48.22566F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.57598F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 18.19836F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(795, 787);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(3, 3);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(789, 373);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            // 
+            // btn_open
+            // 
+            this.btn_open.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_open.Location = new System.Drawing.Point(3, 646);
+            this.btn_open.Name = "btn_open";
+            this.btn_open.Size = new System.Drawing.Size(789, 138);
+            this.btn_open.TabIndex = 1;
+            this.btn_open.Text = "Mở thư mục nhạc";
+            this.btn_open.UseVisualStyleBackColor = true;
+            this.btn_open.Click += new System.EventHandler(this.btn_open_Click);
+            // 
+            // listmusic
+            // 
+            this.listmusic.BackColor = System.Drawing.SystemColors.MenuText;
+            this.listmusic.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listmusic.ForeColor = System.Drawing.Color.Lime;
+            this.listmusic.ItemHeight = 37;
+            this.listmusic.Location = new System.Drawing.Point(3, 382);
+            this.listmusic.Name = "listmusic";
+            this.listmusic.Size = new System.Drawing.Size(789, 258);
+            this.listmusic.TabIndex = 2;
+            this.listmusic.SelectedIndexChanged += new System.EventHandler(this.listmusic_SelectedIndexChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Mp3|*.mp3|Wav|*.wav";
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btn_open;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(791, 781);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "App Nghe Nhạc Offline";
+            this.TransparencyKey = System.Drawing.Color.Transparent;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button btn_open;
+        private System.Windows.Forms.ListBox listmusic;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ DemoAppNgheNhac/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoAppNgheNhac
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        String[] fileNames; //Tên File
+        String[] filePaths; //Đường dẫn File
+        private void btn_open_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                fileNames = openFileDialog1.SafeFileNames;
+                filePaths = openFileDialog1.FileNames;
+                foreach(String fileName in fileNames)
+                {
+                    listmusic.Items.Add(fileName);
+                }
+            }
+        }
+
+        private void listmusic_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = filePaths[listmusic.SelectedIndex];
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
